eba5498be21522bb9d9f3a0d3d3f824bb007c7f2
Ace Card <anonim8301@gmail.com>
2022-06-23 00:37:22 +0300

    Initial

---
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.coroutines
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(3000L)
+            Log.d(TAG, "Hello from the Coroutines, Thread name: ${Thread.currentThread().name} ")
+        }
+        Log.d(TAG, "Hello from the Main thread: ${Thread.currentThread().name} ")
+    }
+}
