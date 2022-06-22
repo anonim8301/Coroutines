397cc8cd41323b53b5b6b41666feb6a73165fb56
Ace Card <anonim8301@gmail.com>
2022-06-23 01:28:29 +0300

    Coroutines Contexts

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.example.coroutines
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.example.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = "MainActivity"
+    lateinit var binding: ActivityMainBinding
 
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        GlobalScope.launch {
-            delay(3000L)
-            Log.d(TAG, "Hello from the Coroutines, Thread name: ${Thread.currentThread().name} ")
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG, "Started in thread: ${Thread.currentThread().name} ")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                binding.tvCenter.text = answer
+                Log.d(TAG, "Text changed in thread: ${Thread.currentThread().name} ")
+            }
         }
         Log.d(TAG, "Hello from the Main thread: ${Thread.currentThread().name} ")
+    }
+
+    suspend fun doNetworkCall(): String {
+        delay(2000L)
+        return "The answer"
     }
 }
